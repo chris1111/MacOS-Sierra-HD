383271996718f76de3e40015ccf7610fd1c4328c
chris1111 <leblond1111@gmail.com>
2016-07-01 23:37:52 -0400

    corrections
    Follow every steps in the Video

---
+++ README.md
@@ -0,0 +1,30 @@
+# MacOS Sierra HD
+
+This a app bundle create with Platypus-5.1
+MacOS Sierra HD
+Based on the old InstaDMG
+https://wiki.afp548.com/index.php/Create_a_Mac_OS_X_with_InstaDMG_and_InstaUp2Date
+ 
+Working for MacOS 10.12  
+
+
+# Instructions
+
+This will Deploy MacOS Sierra on your selected Volume.
+
+You need Installer MacOS Sierra.app from Mac App Store on your Mac.
+
+You need empty SSD OR HD before using this program!
+(The name of your Install Drive must be without any space.` ). 
+
+1.  Download MacOS Sierra HD.app
+
+2.  Open `MacOS Sierra HD.app`
+(Follow every steps` ). 
+ 
+How Does MacOS Sierra HD Work?
+Its a shell script called MacOS-HD.sh (See the scripts` ).
+
+Follow every steps in the Video
+
+[![Modular Image Creation](http://i35.servimg.com/u/f35/18/50/18/69/icon-511.png)](https://www.youtube.com/watch?v=xiwYqvqu4W4)
---
+++ Resources/MacOS-HD.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# MacOS Sierra HD
+# chris1111 http://hackintosh-montreal.com
+
+osascript ./Intro.app
+Sleep 2
+
+#debugging output
+[ -f /tmp/debug ] && set -x 
+
+
+clear;
+echo "**********************************************  "
+echo "Starting MacOS Sierra HD!  "   
+echo "**********************************************  "
+
+
+Sleep 2
+osascript ./main.app
+
+Sleep 3
+
+echo "**********************************************  "
+echo "Mount Installer image!  "
+echo "**********************************************  "
+Sleep 2
+osascript -e 'display notification "MacOS Sierra HD" with title "OS Install Start"  sound name "default"'
+		
+       
+
+
+Sleep 2
+installer -allowUntrusted -verboseR -pkg /Volumes/OS\ X\ Install\ ESD/Packages/OSInstall.mpkg -target /Volumes/MacOS_HD
+
+Sleep 5
+
+echo "**********************************************  "
+echo "Unmount Installer image!  "
+echo "**********************************************  "
+Sleep 2
+hdiutil detach /Volumes/"OS X Install ESD"
+
+Sleep 2
+echo "**********************************************  "
+echo "Volume rename Macintosh-HD  "
+echo "**********************************************  "
+/usr/sbin/diskutil rename "MacOS_HD" "Macintosh-HD"
+
+open /Volumes/"Macintosh-HD"
+
+Sleep 2
+
+
+echo "Enjoy!  " 
